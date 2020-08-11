c4b4e81a16fbb2de99671db3fe5a9b25e56ad190
Andy Davis <andrew.davis@ll.mit.edu>
2020-08-11 16:41:26 -0400

    changes from review

--- src/pandelephant.py
+++ src/pandelephant.py
@@ -9,6 +9,7 @@ class Execution(Base):
     __tablename__ = 'executions'
     execution_id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+    description = Column(String)
     start_time = Column(DateTime(timezone=True))
     end_time = Column(DateTime(timezone=True)) 
     processes = relationship("Process", back_populates="execution")
@@ -28,13 +29,21 @@ class Process(Base):
     __tablename__ = 'processes'
     process_id = Column(Integer, primary_key=True)
     execution_id = Column(Integer, ForeignKey('executions.execution_id'), nullable=False)
-    names = Column(ARRAY(String))
     pid = Column(BigInteger, nullable=False)
     ppid = Column(BigInteger)
+    execution = relationship("Execution", back_populates="processes")
+    threads = relationship("Thread", back_populates="process")
+    modules = relationship("Module", back_populates="process")
+
+class Thread(Base):
+    __tablename__ = 'threads'
+    thread_id = Column(Integer, primary_key=True)
+    process_id = Column(Integer, ForeignKey('processes.process_id'), nullable=False)
+    names = Column(ARRAY(String))
     tid = Column(BigInteger)
     create_time = Column(DateTime(timezone=True), nullable=False)
-    execution = relationship("Execution", back_populates="processes")
-    modules = relationship("Module", back_populates="process")
+    end_time = Column(DateTime(timezone=True))
+    process = relationship("Process", back_populates="threads")
 
 class Module(Base):
     __tablename__ = 'modules'
@@ -42,12 +51,18 @@ class Module(Base):
     process_id = Column(Integer, ForeignKey('processes.process_id'), nullable=False)
     name = Column(String)
     path = Column(String)
-    asid = Column(BigInteger, nullable=False)  # bases are virtual addresses so we need an ASID
-    execution_offset = Column(BigInteger, nullable=False) # We're using an ASID so we need a "time". We're using instruction count indexed by execution start (execution starts at 0).
-    base = Column(BigInteger, nullable=False) 
+    base_id = Column(Integer, ForeignKey('virtual_addresses.address_id'), nullable=False)
+    base = relationship("VirtualAddress", uselist=False)
     size = Column(BigInteger, nullable=False)
     process = relationship("Process", back_populates="modules")
     codepoints = relationship("CodePoint", back_populates="module")
+
+class VirtualAddress(Base):
+    __tablename__ = 'virtual_addresses'
+    address_id = Column(Integer, primary_key=True)
+    asid = Column(BigInteger, nullable=False)  # bases are virtual addresses so we need an ASID
+    execution_offset = Column(BigInteger, nullable=False)  # We're using an ASID so we need a "time". We're using instruction count indexed by execution start (execution starts at 0).
+    address = Column(BigInteger, nullable=False)
 
 class CodePoint(Base):
     __tablename__ = 'codepoints'
@@ -57,16 +72,22 @@ class CodePoint(Base):
     module = relationship("Module", back_populates="codepoints", uselist=False)
 
 
-class TaintFlow(Base):
-    __tablename__ = 'taint_flows'
-    taint_flow_id = Column(Integer, primary_key=True)
+class WriteReadFlow(Base):
+    __tablename__ = 'writeread_flows'
+    writeread_flow_id = Column(Integer, primary_key=True)
     # this is the code location of the write (store)
     write_id = Column(Integer, ForeignKey('codepoints.code_point_id'), nullable=False)
+    write_thread_id = Column(Integer, ForeignKey('threads.thread_id'), nullable=False)
+
     # this is the code location of the read (load)
     read_id = Column(Integer, ForeignKey('codepoints.code_point_id'), nullable=False)
+    read_thread_id = Column(Integer, ForeignKey('threads.thread_id'), nullable=False)
+
     execution_offset = Column(BigInteger, nullable=False) # This is an offset in the exection by 
     write = relationship('CodePoint', foreign_keys=[write_id], uselist=False)
+    write_thread = relationship('Thread', foreign_keys=[write_thread_id], uselist=False)
     read = relationship('CodePoint', foreign_keys=[read_id], uselist=False)
+    read_thread = relationship('Thread', foreign_keys=[read_thread_id], uselist=False)
 
 def create_session(url, debug=False):
     engine = create_engine(url, echo=debug)
